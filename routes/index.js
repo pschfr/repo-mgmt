da6700a619efb356bf5b19be948a979b978fb309
Paul Schaefer <yeppaul2@gmail.com>
2019-04-04 19:49:20 -0400

    First commit with Octokit and simple loop over repos!

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+var Octokit = require('@octokit/rest');
+var octokit = new Octokit();
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  
+  /**
+   * Connect to GitHub and get our repositories!
+   * Using the paginate endpoint gives us all the repos at once,
+   * rather than the manual pagination the listForUser endpoint returns.
+   */
+  octokit.paginate('GET /users/:username/repos', { username: 'pschfr' }).then(data => {
+    // console.log(data)
+    // Render the page
+    res.render('index', { title: 'Repo Management', repos: data });
+  })
 });
 
 module.exports = router;
